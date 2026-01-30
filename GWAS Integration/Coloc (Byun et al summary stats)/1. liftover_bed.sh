79ac150e31887068968b4dee1796a401bbf24502
ttluong1 <52212680+ttluong1@users.noreply.github.com>
2026-01-30 13:40:00 -0500

    Comment out liftOver commands in liftover_bed.sh
    Commented out liftOver commands for specific bed files.

--- GWAS Integration/Coloc (Byun et al summary stats)/1. liftover_bed.sh
+++ GWAS Integration/Coloc (Byun et al summary stats)/1. liftover_bed.sh
@@ -2,11 +2,10 @@
 #Jan 16h 2025
 #sbatch --partition=quick  -c4 --mem=50g --time=03:00:00 liftover_bed.sh
 
-
 module load ucsc
 
 liftOver hg19_total.bed hg19ToHg38.over.chain.gz hg38_total.bed total_unlifted.bed
-liftOver hg19_luad.bed hg19ToHg38.over.chain.gz hg38_luad.bed luad_unlifted.bed
-liftOver hg19_lusc.bed hg19ToHg38.over.chain.gz hg38_lusc.bed lusc_unlifted.bed
-liftOver hg19_scc.bed hg19ToHg38.over.chain.gz hg38_scc.bed scc_unlifted.bed
+#liftOver hg19_luad.bed hg19ToHg38.over.chain.gz hg38_luad.bed luad_unlifted.bed
+#liftOver hg19_lusc.bed hg19ToHg38.over.chain.gz hg38_lusc.bed lusc_unlifted.bed
+#liftOver hg19_scc.bed hg19ToHg38.over.chain.gz hg38_scc.bed scc_unlifted.bed
 
