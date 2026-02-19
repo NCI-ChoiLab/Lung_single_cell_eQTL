b02fecf27e0150e41b276f3ea44cdd62e5b36ceb
ttluong1 <52212680+ttluong1@users.noreply.github.com>
2026-02-19 15:35:08 -0500

    Refactor data processing in Figure 6d script
    Refactor code to streamline data processing and remove unused variables.

--- 4. Figures/Figure 6d.R
+++ 4. Figures/Figure 6d.R
@@ -18,11 +18,7 @@ library(org.Hs.eg.db)
 setwd('/data/Choi_lung/TTL/Figs_for_manuscript/ROS1_DEG/')
 
 sc = readRDS('/data/Choi_lung/TTL/Clinical_eQTL/total_with_preinfo.rds')
-rel = subset(sc, cell_types %in% c('Alveolar Transitional Cells', 'AT2', 'Secretory Transitional Cells'))
-alv_trans = subset(rel, cell_types == 'Alveolar Transitional Cells')
-at2 = subset(rel, cell_types == 'AT2')
-alv_cells = subset(rel, cell_types != 'Secretory Transitional Cells')
-sec_trans = subset(rel, cell_types == 'Secretory Transitional Cells')
+at2 = subset(sc, cell_types == 'AT2')
 
 get_matrix = function(s){
   s[['RNA']] = JoinLayers(s[['RNA']])
@@ -37,7 +33,7 @@ get_matrix = function(s){
   sce2 = SingleCellExperiment(assays = list(counts = counts),
                               colData = meta)
   
-  sce2$orig.ident = factor(sce2$Sample)
+  sce2$Sample = factor(sce2$Sample)
   e = sce2
   #telling what to aggregate by
   groups = colData(e)[, c('Sample')]
@@ -53,11 +49,7 @@ get_matrix = function(s){
   return(expression_sum)
 }
 
-rel_matrix = get_matrix(rel)
 at2_matrix = get_matrix(at2)
-alv_trans_matrix = get_matrix(alv_trans)
-alv_cells_matrix = get_matrix(alv_cells)
-sec_trans_matrix = get_matrix(sec_trans)
 
 get_deg = function(mat){
   coldata = as.data.frame(NormalizeData(mat))
@@ -77,38 +69,9 @@ get_deg = function(mat){
   return(gene_deg)
 }
 
-rel_deg = get_deg(rel_matrix)
-alv_deg = get_deg(alv_trans_matrix)
 at2_deg = get_deg(at2_matrix)
-alv_cells_deg = get_deg(alv_cells_matrix)
-sec_deg = get_deg(sec_trans_matrix)
 
-
-get_deg2 = function(mat){
-  coldata = as.data.frame(NormalizeData(mat))
-  coldata = as.data.frame(t(coldata['ROS1',]))
-  coldata$q = coldata %>% mutate(q = ntile(ROS1,4)) %>% pull(q)
-  coldata = subset(coldata , q %in% c('1','4'))
-  coldata$q = factor(coldata$q, levels = c('1','4'))
-  
-  cts = mat[,rownames(coldata)]
-  dds = DESeq2::DESeqDataSetFromMatrix(countData = cts,
-                                       colData = coldata,
-                                       design = ~q)
-  dds_finals = DESeq2::DESeq(dds)
-  dds_results = DESeq2::results(dds_finals)
-  gene_deg = as.data.frame(dds_results)
-  gene_deg = gene_deg[order(gene_deg$log2FoldChange, decreasing = T),]
-  return(gene_deg)
-}
-
-
-
-#saveRDS(rel_deg, 'rel_deg.rds')
-#saveRDS(alv_deg, 'alv_deg.rds')
 #saveRDS(at2_deg,'at2_deg.rds')
-#saveRDS(alv_cells_deg, 'alv_cells_deg.rds')
-#saveRDS(sec_deg,'sec_deg.rds')
 
 
 save_deg = function(lung.expr, f){
@@ -119,17 +82,10 @@ save_deg = function(lung.expr, f){
   write.table(deg, f, sep = '\t', row.names = T, col.names = T, quote = F)
 }
 
-save_deg(rel_deg, 'pseudobulk_rel_deg.tsv')
-save_deg(alv_deg, 'pseudobulk_alv_deg.tsv')
-save_deg(at2_deg, 'pseudobulk_at2_deg.tsv')
-save_deg(alv_cells_deg, 'pseudobulk_alv_cells_deg.tsv')
-save_deg(sec_deg, 'pseudobulk_sec_deg.tsv')
+#save_deg(at2_deg, 'pseudobulk_at2_deg.tsv')
 
-rel_deg = readRDS('rel_deg.rds')
-alv_deg = readRDS('alv_deg.rds')
 at2_deg = readRDS('at2_deg.rds')
-alv_cells_deg = readRDS('alv_cells_deg.rds')
-sec_deg = readRDS('sec_deg.rds')
+
 
 get_pathway = function(deg){
   lung.expr = deg
@@ -155,11 +111,7 @@ get_pathway = function(deg){
   return(gwp.sig.lung.expr)
 }
 
-rel_path = get_pathway(rel_deg)
-alv_path = get_pathway(alv_deg)
 at2_path = get_pathway(at2_deg)
-alv_cells_path = get_pathway(alv_cells_deg)
-sec_path = get_pathway(sec_deg)
 
 library(enrichplot)
 library(DOSE)
